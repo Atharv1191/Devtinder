818b7367d5a32ad6481b5420876429b9cc885bf7
Atharv Joshi <atharvj07@gmail.com>
2024-09-26 15:52:59 +0530

    "Updated user schema with additional validation and fields"

--- models/user.js
+++ models/user.js
@@ -2,22 +2,47 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:30,
     },
     LasttName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
         type:Number,
+        min:18
     },
     gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+
+        }
+    },
+    photoUrl:{
         type:String
     },
-})
+    about:{
+        type:String,
+        default:"This is a default about of the user",
+    },
+    skills:{
+        type:[String]
+    }
+},{timestamps:true})
 module.exports = mongoose.model("User", userSchema);
